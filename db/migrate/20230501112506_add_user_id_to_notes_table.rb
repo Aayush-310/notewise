5964feddc7acaf970cfd9cb5f441729c764c2fb1
Aayush-310 <aayushp2011@gmail.com>
2023-05-01 17:43:29 +0545

    pending migration done -two new articles added user/index.html edited to show user notes -added signout button to the index page of notes

--- db/migrate/20230501112506_add_user_id_to_notes_table.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToNotesTable < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :notes_tables, :user, null: false, foreign_key: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_112114) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_112506) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", default: 0, null: false
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_112114) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "notes", "users"
 end
